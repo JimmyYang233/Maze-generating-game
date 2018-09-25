26c896c8846cc5253eb8b56c00bb5736489cf049
Dan Ning Yang <ydn2012@hotmail.com>
2018-09-24 20:19:43 -0400

    first push

---
+++ Assets/Script/Player.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+	{
+		float xAxis = Input.GetAxis("Horizontal");
+		float zAxis = Input.GetAxis("Vertical");
+
+		Vector3 position = transform.position;
+		Vector3 newPosition = new Vector3(position.x + xAxis, position.y, position.z + zAxis);
+
+		transform.position = newPosition;
+	}
+}
