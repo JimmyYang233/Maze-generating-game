76c79adb11ff38256b6ce9259b326c380178f281
Dan Ning <ydn2012@hotmail.com>
2018-09-24 23:23:33 -0400

    2nd push

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -6,21 +6,68 @@ public class Player : MonoBehaviour
 {
 
 	public float speed;
+	public float mouseSensitivity;
+
+	private GameObject camera;
+	private float xAxisClamp;
+
+	private CharacterController charController;
 	// Use this for initialization
 	void Start ()
 	{
-		
+		camera = transform.GetChild(0).gameObject;
+		charController = gameObject.GetComponent<CharacterController>();
+		xAxisClamp = 0;
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate ()
 	{
+		CameraMovement();
+		CameraRotation();
+	}
+
+	private void CameraRotation()
+	{
+		float mouseX = Input.GetAxis("Mouse X")*mouseSensitivity;
+		float mouseY = Input.GetAxis("Mouse Y")*mouseSensitivity;
+
+		xAxisClamp += mouseY;
+
+		if (xAxisClamp > 90.0f)
+		{
+			xAxisClamp = 90.0f;
+			mouseY = 0.0f;
+			ClampXAxisRotationToValue(270.0f);
+		}
+		
+		else if (xAxisClamp < -90.0f)
+		{
+			xAxisClamp = -90.0f;
+			mouseY = 0.0f;
+			ClampXAxisRotationToValue(90.0f);
+		}
+		
+		camera.transform.Rotate(Vector3.left*mouseY);
+		transform.Rotate(Vector3.up*mouseX);
+		
+		Debug.Log(mouseX + ", " + mouseY);
+	}
+
+	private void CameraMovement()
+	{
 		float xAxis = Input.GetAxis("Horizontal");
 		float zAxis = Input.GetAxis("Vertical");
 
-		Vector3 position = transform.position;
-		Vector3 newPosition = new Vector3(position.x + xAxis*speed, position.y, position.z + zAxis*speed);
+		Vector3 newPosition = transform.forward * zAxis * speed+ transform.right*xAxis*speed;
 
-		transform.position = newPosition;
+		charController.SimpleMove(newPosition);
+	}
+
+	private void ClampXAxisRotationToValue(float value)
+	{
+		Vector3 eulerRotation = camera.transform.eulerAngles;
+		eulerRotation.x = value;
+		camera.transform.eulerAngles = eulerRotation;
 	}
 }
